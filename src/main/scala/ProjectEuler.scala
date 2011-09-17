dc0ea123e5e4b43aa77eb4f651dbae8b722bffad
Olivier Bruchez <olivier@bruchez.org>
2011-09-17 19:17:09 +0200

    Add ProjectEuler main object

--- src/main/scala/ProjectEuler.scala
+++ src/main/scala/ProjectEuler.scala
@@ -2,6 +2,15 @@ package projecteuler
 
 object ProjectEuler {
   def main(args: Array[String]) {
-    println("Hello world")
+    if (args.length != 1) {
+      println("Usage: ProjectEuler <problem number>")
+    } else {
+      try {
+        Class.forName("projecteuler.P"+args(0)).newInstance.asInstanceOf[{ def solve(): Unit }].solve()
+      } catch {
+        case e: Exception =>
+          println("Problem "+args(0)+" not found ("+e+")")
+      }
+    }
   }
 }
